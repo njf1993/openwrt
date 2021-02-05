c741a919a9294addd81d6c4788c08940d92c4807
ni jiangfeng <njf550368984@gmail.com>
2021-02-05 23:15:37 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,10 +17,15 @@
 # git clone https://github.com/xiaorouji/openwrt-passwall.git package/passwall
 
 # 添加bypass
-git clone --depth=1 https://github.com/garypang13/luci-app-bypass
+git clone --depth=1 https://github.com/garypang13/luci-app-bypass.git package/luci-app-bypass
+
+# 添加bypass 依赖
+svn co https://github.com/garypang13/openwrt-packages/trunk/smartdns package/smartdns
+svn co  https://github.com/garypang13/openwrt-packages/trunk/tcping package/tcping
+svn co  https://github.com/garypang13/openwrt-packages/trunk/lua-maxminddb package/lua-maxminddb
 
 # 添加dnsfilter
-git clone --depth=1 https://github.com/garypang13/luci-app-dnsfilter
+git clone --depth=1 https://github.com/garypang13/luci-app-dnsfilter.git package/luci-app-dnsfilter
 
 # 添加luci-theme-argon
 rm -rf ../lean/luci-theme-argon
