b419664772d8d1f7b805bcdfa20d50601525ec3a
ni jiangfeng <njf550368984@gmail.com>
2021-02-06 23:58:52 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,20 +14,20 @@
 # git clone https://github.com/fw876/helloworld.git package/luci-app-ssr-plus
 
 # 添加PassWall
-# git clone https://github.com/xiaorouji/openwrt-passwall.git package/passwall
+git clone https://github.com/xiaorouji/openwrt-passwall.git package/passwall
 
 # 添加bypass
-git clone https://github.com/garypang13/luci-app-bypass.git package/luci-app-bypass
+# git clone https://github.com/garypang13/luci-app-bypass.git package/luci-app-bypass
 
 # 添加bypass 依赖
-svn co https://github.com/garypang13/openwrt-packages/trunk/smartdns package/smartdns
-svn co https://github.com/garypang13/openwrt-packages/trunk/tcping package/tcping
-svn co https://github.com/garypang13/openwrt-packages/trunk/lua-maxminddb package/lua-maxminddb
-find package/*/ feeds/*/ -maxdepth 2 -path "*luci-app-bypass/Makefile" | xargs -i sed -i 's/shadowsocksr-libev-ssr-redir/shadowsocksr-libev-alt/g' {}
-find package/*/ feeds/*/ -maxdepth 2 -path "*luci-app-bypass/Makefile" | xargs -i sed -i 's/shadowsocksr-libev-ssr-server/shadowsocksr-libev-server/g' {}
+# svn co https://github.com/garypang13/openwrt-packages/trunk/smartdns package/smartdns
+# svn co https://github.com/garypang13/openwrt-packages/trunk/tcping package/tcping
+# svn co https://github.com/garypang13/openwrt-packages/trunk/lua-maxminddb package/lua-maxminddb
+# find package/*/ feeds/*/ -maxdepth 2 -path "*luci-app-bypass/Makefile" | xargs -i sed -i 's/shadowsocksr-libev-ssr-redir/shadowsocksr-libev-alt/g' {}
+# find package/*/ feeds/*/ -maxdepth 2 -path "*luci-app-bypass/Makefile" | xargs -i sed -i 's/shadowsocksr-libev-ssr-server/shadowsocksr-libev-server/g' {}
 
 # 添加dnsfilter
-git clone https://github.com/garypang13/luci-app-dnsfilter.git package/luci-app-dnsfilter
+# git clone https://github.com/garypang13/luci-app-dnsfilter.git package/luci-app-dnsfilter
 
 # 添加luci-theme-argon
 rm -rf ../lean/luci-theme-argon
