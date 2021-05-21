6a6616eb8baef680abdb7cd9831836c530715ce5
ni jiangfeng <njf550368984@gmail.com>
2021-05-21 13:08:54 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,7 +14,7 @@
 # git clone https://github.com/fw876/helloworld.git package/luci-app-ssr-plus
 
 # 添加PassWall
-# git clone https://github.com/xiaorouji/openwrt-passwall.git package/passwall
+git clone https://github.com/xiaorouji/openwrt-passwall.git package/passwall
 
 # 添加bypass
 git clone https://github.com/garypang13/luci-app-bypass package/luci-app-bypass
